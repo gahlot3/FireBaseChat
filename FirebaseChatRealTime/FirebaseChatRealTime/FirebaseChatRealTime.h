902e85d71e9c462f6a605484f72b17fbfb485af1
kamlesh <kamleshgahlot19@gmail.com>
2023-08-25 14:26:15 +0530

    initial commit

---
+++ FirebaseChatRealTime/FirebaseChatRealTime/FirebaseChatRealTime.h
@@ -0,0 +1,18 @@
+//
+//  FirebaseChatRealTime.h
+//  FirebaseChatRealTime
+//
+//  Created by Emizen on 25/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirebaseChatRealTime.
+FOUNDATION_EXPORT double FirebaseChatRealTimeVersionNumber;
+
+//! Project version string for FirebaseChatRealTime.
+FOUNDATION_EXPORT const unsigned char FirebaseChatRealTimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseChatRealTime/PublicHeader.h>
+
+
